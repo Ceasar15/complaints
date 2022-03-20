99c8369f8da5640c03c4222449b553f5eb215ba7
Ceasar <ceasarkwadwo@gmail.com>
2022-03-20 15:32:05 +0000

    authentication

--- complaints/urls.py
+++ complaints/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
@@ -7,11 +7,15 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'auth/',
+        include('apps.Auth.urls')
+    ),
+
 ]
 
 if settings.DEBUG:

5755c76c3adb59b854be8a69e7f2466959191742
Ceasar <ceasarkwadwo@gmail.com>
2022-03-20 16:38:57 +0000

    django heroku dependency

--- complaints/settings.py
+++ complaints/settings.py
@@ -1,4 +1,4 @@
-import django_heroku
+# import django_heroku
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -149,4 +149,4 @@ SIMPLE_JWT = {
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())

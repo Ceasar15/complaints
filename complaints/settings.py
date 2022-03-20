a422ebde396e3b89ef76748b6e624e982100f24e
Ceasar <ceasarkwadwo@gmail.com>
2022-03-20 15:52:21 +0000

    deploy to heroku

--- complaints/settings.py
+++ complaints/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from datetime import timedelta
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5mcq)xgqq04%nn_qjo7d#s1+ak9r=w&%+i*4#@4jq9#1dqpi@4'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

029fda3731c081aaddb4238935836593872adb80
Ceasar <ceasarkwadwo@gmail.com>
2022-03-22 17:27:41 +0000

    profile page updated

--- apps/Auth/urls.py
+++ apps/Auth/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from apps.Auth.views import MyObtainTokenPairView, RegisterView, ProfileViewPost, ProfileViewGet
 from rest_framework_simplejwt.views import TokenRefreshView
 
-
 urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- apps/Auth/views.py
+++ apps/Auth/views.py
@@ -50,4 +50,3 @@ class ProfileViewGet(generics.UpdateAPIView):
     queryset = Profile.objects
     lookup_field = 'user_id'
 
-
